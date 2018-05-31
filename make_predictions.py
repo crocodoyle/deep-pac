66a3ef2b9f5d24f16ac1af75615eda80fd13b4eb
Andrew Doyle <doyle.andrew@gmail.com>
2018-05-31 10:55:08 -0400

    bug

--- make_predictions.py
+++ make_predictions.py
@@ -51,31 +51,38 @@ if __name__ == '__main__':
     pacwriter = csv.writer(pac_file)
 
     pacwriter.writerow(['PAC_ID', 'Prediction'])
-    for subj in subjects:
 
-        img = nib.load(data_dir + subj['id'] + '.nii').get_data()
-        meta = np.empty((1, 7))
-        meta[0, 0:3] = to_categorical(subj['site'] - 1, num_classes=3)
-        meta[0, 3] = subj['age'] / 100
-        meta[0, 4:6] = to_categorical(subj['gender'] - 1, num_classes=2)
-        meta[0, 6] = subj['tiv'] / 2000
+    predictions = np.empty((num_models, len(subjects)))
 
-        predictions = []
-        for model_num in range(num_models):
-            model_filename = 'pac_model_' + str(model_num) + '.hdf5'
-            model = load_model(data_dir + model_filename)
+    for model_num in range(num_models):
+        model_filename = 'pac_model_' + str(model_num) + '.hdf5'
+        model = load_model(data_dir + model_filename)
+
+        print('model', model_num)
+        for j, subj in enumerate(subjects):
+
+            img = nib.load(data_dir + subj['id'] + '.nii').get_data()
+            meta = np.empty((1, 7))
+            meta[0, 0:3] = to_categorical(subj['site'] - 1, num_classes=3)
+            meta[0, 3] = subj['age'] / 100
+            meta[0, 4:6] = to_categorical(subj['gender'] - 1, num_classes=2)
+            meta[0, 6] = subj['tiv'] / 2000
 
             prediction = model.predict([img[np.newaxis, ..., np.newaxis], meta])[0]
-            predictions.append(prediction[1])
+            predictions[model_num, j] = prediction[1]
 
-        print('Predictions:', predictions, np.sum(predictions) / num_models)
+        K.clear_session()
 
-        if np.sum(predictions) / num_models >= 0.5:
+    for i in range(len(subjects)):
+        subj_pred = predictions[:, i]
+
+        print('Predictions:', subj_pred, np.sum(subj_pred) / num_models)
+
+        if np.sum(subj_pred) / num_models >= 0.5:
             final_prediction = 1
         else:
             final_prediction = 0
 
-        pacwriter.writerow([subj['id'], final_prediction])
+        pacwriter.writerow([subjects[i]['id'], final_prediction])
 
     pac_file.close()
-    K.clear_session()
